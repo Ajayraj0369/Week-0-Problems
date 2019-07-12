3d415859b871a058cbba56bc57004712df08d4ae
Ajayraj0369 <47928104+Ajayraj0369@users.noreply.github.com>
2019-07-12 20:21:00 +0530

    Week Zero Problems

---
+++ check Vowel.jss.js
@@ -0,0 +1,3 @@
+function isVowel(char){   if(char === 'a' || char === 'i' || char === 'e' || char === 'o'|| char === 'u'){     console.log("wow its a vowel") } else console.log("its not a vowel") } 
+ 
+isVowel("g") 
---
+++ swap two arrays.jss.js
@@ -0,0 +1,5 @@
+function swap(x,y){   let a = x;   x = y;   console.log(x) console.log(a)} 
+ 
+x = [2,34,5] y = [4,7,8] swap(x,y) 
+ 
+ 
